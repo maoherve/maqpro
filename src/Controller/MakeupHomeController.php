f807d5bb63df94c474f8256344ab284157978eda
Mao Mater <maoherve8@gmail.com>
2021-08-15 17:27:28 +0200

    test

--- src/Controller/MakeupHomeController.php
+++ src/Controller/MakeupHomeController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Makeup;
+use App\Entity\MakeupProducts;
+use App\Repository\MakeupRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -32,4 +34,25 @@ class MakeupHomeController extends AbstractController
             'makeups' => $makeups
         ]);
     }
+
+
+    /**
+     * @param MakeupRepository $seasonRepository
+     * @param int $makeup_id
+     * @return Response
+     * @Route("/show-by-season/{makeup_id}", name="Products")
+     */
+    public function showBySeason(MakeupRepository $seasonRepository, int $makeup_id): Response
+    {
+        $makeup = $seasonRepository->findOneBy(['id' => $makeup_id]);
+
+        return $this->render('makeup_home/makeupDetails.html.twig',
+            ['makeupProducts' => $makeup->getMakeupProducts(),
+                'makeupName' => $makeup->getName()
+                ]);
+    }
+
+
+
+
 }
